87c0a0f2bd31e0551caaf1fcaee69284cac148e2
Kendra <kendra.jordan8@gmail.com>
2018-10-15 20:07:21 -0400

    added sentence , word, and character counts

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@ var app = new Vue({
     data:{
       textbox:"",
       paragraphs:0,
+      words:0,
+      characters:0,
+      sentences:0
     },
     methods:{
       paragraphCount: function (){
@@ -10,10 +13,27 @@ var app = new Vue({
         let paragraphReg =/\s{2}\b[A-Z0-9]|^\b[A-Z0-9]/g;
         let found = passage.match(paragraphReg);
         this.paragraphs=found.length;
-        sentenceCount();
+        this.wordCount();
+        this.characterCount();
+        this.sentenceCount();
+      },
+      wordCount: function(){
+        let passage = this.textbox;
+        let wordReg = /(Dr\.?)|([Mr?\.?s?\.?)|([A-Za-z'])+/g;
+        let found = passage.match(wordReg);
+        this.words = found.length;
+      },
+      characterCount: function(){
+        let passage = this.textbox;
+        let characterReg = /.|\s/g;
+        let found = passage.match(characterReg);
+        this.characters = found.length;
       },
       sentenceCount: function(){
-
+        let passage =this.textbox;
+        let sentenceReg=/[a-zA-Z0-9\s,\-@#$%^&*()_+=[{\]|\\;:"<>]+/g
+        let found = passage.match(sentenceReg);
+        this.sentences=found.length;
       }
 
     }
