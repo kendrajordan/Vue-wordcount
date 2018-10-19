776195acb904b2e6c7053739919ced5856721f0d
Kendra <kendra.jordan8@gmail.com>
2018-10-18 23:07:06 -0400

    finished vue count

--- js/main.js
+++ js/main.js
@@ -5,7 +5,25 @@ var app = new Vue({
       paragraphs:0,
       words:0,
       characters:0,
-      sentences:0
+      sentences:0,
+      longestword:0,
+
+
+    },
+    computed:{
+      popularcharacter(){
+        var passage = this.textbox;
+
+        var max = 0;
+        var maxChar =''
+         passage.split("").forEach(function(char){
+          if(passage.split(char).length > max){
+            max = passage.split(char).length;
+            maxChar = char;
+          }
+        });
+        return maxChar;
+      }
     },
     methods:{
       paragraphCount: function (){
@@ -16,12 +34,14 @@ var app = new Vue({
         this.wordCount();
         this.characterCount();
         this.sentenceCount();
+        this.longestWordCount();
       },
       wordCount: function(){
         let passage = this.textbox;
-        let wordReg = /(Dr\.?)|([Mr?\.?s?\.?)|([A-Za-z'])+/g;
+        let wordReg = /(Dr\.?)|(Mr?\.?s?\.?[a-z]+)|([A-Za-z'’-])+/g;
         let found = passage.match(wordReg);
         this.words = found.length;
+
       },
       characterCount: function(){
         let passage = this.textbox;
@@ -34,7 +54,17 @@ var app = new Vue({
         let sentenceReg=/[a-zA-Z0-9\s,\-@#$%^&*()_+=[{\]|\\;:"<>]+/g
         let found = passage.match(sentenceReg);
         this.sentences=found.length;
-      }
+      },
+      longestWordCount: function(){
+        let passage = this.textbox;
+        let lowercase = passage.toLowerCase();
+        let wordReg = /(Dr\.?)|(Mr?\.?s?\.?[a-z]+)|([A-Za-z'’-])+/g;
+        let found = lowercase.match(wordReg);
+        let longest = found.sort(function (a, b) { return b.length - a.length; })[0];
+
+        this.longestword= longest;
+      },
+
 
     }
 })
